63a89f875eff284076a658a9ec294ad095c977eb
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2021-05-22 10:10:04 +0200

    Update c210818610.lua

--- script/c210818610.lua
+++ script/c210818610.lua
@@ -44,10 +44,11 @@ function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
 	end
 end
 function s.activate(e, tp, eg, ep, ev, re, r, rp)
+	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(s.cfilter, tp, LOCATION_MZONE, 0, nil)
 	local ct=#g
 	if ct>=1 then
-        local e1=Effect.CreateEffect(e:GetHandler())
+        local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_FIELD)
         e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
         e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -55,7 +56,7 @@ function s.activate(e, tp, eg, ep, ev, re, r, rp)
         e1:SetValue(1)
         e1:SetReset(RESET_PHASE+PHASE_END)
         Duel.RegisterEffect(e1, tp)
-        local e2=Effect.CreateEffect(e:GetHandler())
+        local e2=Effect.CreateEffect(c)
         e2:SetType(EFFECT_TYPE_FIELD)
         e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
         e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
@@ -70,7 +71,7 @@ function s.activate(e, tp, eg, ep, ev, re, r, rp)
 		local tc=g:GetFirst()
 		for tc in aux.Next(g) do
 			--Immune
-			local e3=Effect.CreateEffect(e:GetHandler())
+			local e3=Effect.CreateEffect(c)
 			e3:SetDescription(3110)
 			e3:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 			e3:SetType(EFFECT_TYPE_SINGLE)
