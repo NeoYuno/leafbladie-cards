7bec87c8dd15d79621b6e1731d4ac07994e6b56d
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2021-09-21 12:58:32 +0200

    Add files via upload

--- script/Apophis/c210030003.lua
+++ script/Apophis/c210030003.lua
@@ -68,7 +68,7 @@ function s.acttg(e,c)
 	return c:GetType()==TYPE_TRAP+TYPE_CONTINUOUS and (c:IsSetCard(0xf100) or c:IsCode(table.unpack(TrapMonster)))
 end
 function s.cfilter(c,e,tp,rp)
-	if c:IsFacedown() or not c:IsCode(89194033) or not c:IsAttackAbove(4000) or not c:IsAbleToGraveAsCost() then return false end
+	if c:IsFacedown() or not c:IsCode(89194033,210030008) or not c:IsAttackAbove(4000) or not c:IsAbleToGraveAsCost() then return false end
 	return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_EXTRA,0,1,nil,e,tp,rp,Group.FromCards(c,e:GetHandler()))
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
