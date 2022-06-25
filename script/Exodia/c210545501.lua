f226c8d665b7d1e60e2c663c3209f3599c472cc5
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2022-06-25 16:32:31 +0200

    Script Update
    Updated the script of "Ritual of the Ultimate Forbidden Lord"

--- script/Exodia/c210545501.lua
+++ script/Exodia/c210545501.lua
@@ -11,14 +11,6 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-    --Win
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetOperation(s.winop)
-	c:RegisterEffect(e2)
 end
 s.listed_names={id,13893596}
 s.listed_series={0x40}
@@ -39,9 +31,9 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_HAND+LOCATION_DECK,0,nil)
 	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
-        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
     Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_HAND+LOCATION_DECK)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND+LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -49,19 +41,72 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local rg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE)
     if Duel.SendtoGrave(rg,REASON_EFFECT)>0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+        local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
         if Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)>0 then
-            --Multi-attack
-            local e1=Effect.CreateEffect(c)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_ATTACK_ALL)
-            e1:SetValue(1)
-            tc:RegisterEffect(e1)
+		    --Unaffected by opponent's card effects
+			local e1=Effect.CreateEffect(tc)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_IMMUNE_EFFECT)
+			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetValue(s.efilter)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			--Cannot be destroyed by battle once per turn
+			local e2=Effect.CreateEffect(tc)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+			e2:SetCountLimit(1)
+			e2:SetValue(s.valcon)
+			tc:RegisterEffect(e2)
+			--Redirect to deck
+			local e3=Effect.CreateEffect(tc)
+			e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e3:SetCode(EVENT_TO_GRAVE)
+			e3:SetRange(LOCATION_MZONE)
+			e3:SetCondition(s.retcon)
+			e3:SetOperation(s.retop)
+			tc:RegisterEffect(e3)
+			--Win
+			local e4=Effect.CreateEffect(tc)
+			e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e4:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
+			e4:SetCode(EVENT_TO_GRAVE)
+			e4:SetRange(LOCATION_MZONE)
+			e4:SetOperation(s.winop)
+			tc:RegisterEffect(e4)
+			local e5=e4:Clone()
+			e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+			e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+			tc:RegisterEffect(e5)
         end
     end
 end
+function s.efilter(e,re)
+	return e:GetOwnerPlayer()==1-re:GetOwnerPlayer()
+end
+function s.valcon(e,re,r,rp)
+	return (r&REASON_BATTLE)~=0
+end
+function s.cfilter(c)
+	return c:IsSetCard(0x40) and c:IsMonster()
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	for tc in aux.Next(eg) do
+		if tc:IsReason(REASON_EFFECT) and not (re and re:GetHandler()==e:GetHandler()) then
+			Duel.SendtoDeck(tc,nil,2,REASON_EFFECT+REASON_REDIRECT)
+		end
+	end
+end
+
 function s.filter(c)
-	return c:IsSetCard(0x40) and c:IsType(TYPE_MONSTER) and c:GetReasonEffect():GetHandler():IsCode(id,13893596)
+	return c:IsSetCard(0x40) and c:IsMonster()
 end
 function s.winop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
