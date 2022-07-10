9380e2f8fa8d8533b2dde4b8545557c9b3073f69
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2022-07-10 18:36:58 +0200

    Add files via upload

--- script/Destiny-HERO/c210480003.lua
+++ script/Destiny-HERO/c210480003.lua
@@ -137,5 +137,16 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(s.eqlimit)
 		e1:SetLabelObject(tc)
 		c:RegisterEffect(e1)
+		if tc:IsLevelAbove(8) or tc:IsCode(76263644) then
+            local e1=Effect.CreateEffect(c)
+            e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+            e1:SetType(EFFECT_TYPE_QUICK_O)
+            e1:SetCode(EVENT_FREE_CHAIN)
+            e1:SetRange(LOCATION_MZONE)
+            e1:SetCountLimit(1)
+            e1:SetTarget(s.destg)
+            e1:SetOperation(s.desop)
+            tc:RegisterEffect(e1)
+        end
 	end
 end
